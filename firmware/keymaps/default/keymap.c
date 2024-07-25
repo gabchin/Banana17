088e1f0b0b3639cf6b6f35c8f2770fedb0f24ff5
gabchin <gabchinn2759@gmail.com>
2024-07-25 15:56:26 +1000

    Functional keymap with 3 buttons and a rotary encoder
    Importing a functional keymap with 3 buttons and a rotary encoder from previous tests.

---
+++ firmware/keymaps/default/keymap.c
@@ -0,0 +1,16 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_MUTE,   KC_A,   KC_B
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = { ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP)}
+};
+#endif
